d0b4c2878ae887bb5238e45cd02e17d921e78290
ruanhong95 <1530771850@qq.com>
2018-01-18 21:40:14 +0800

    我的一个测试文件

---
+++ text/src/text/Text.java
@@ -0,0 +1,12 @@
+package text;
+
+public class Text {
+	public static void main(String[] args){
+  int a=10,b=3,m=5;
+  if(a==b)
+	  m+=a;
+  else m=++a*m;
+  System.out.println(Math.round(11.5));
+  System.out.println(Math.round(-11.5));
+	}
+}
